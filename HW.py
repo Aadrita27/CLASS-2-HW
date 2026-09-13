036e8b007989c07d126164e3005729fd1cb99188
aadrita27 <22priyam@gmail.com>
2026-09-13 18:38:13 +0530

    INITIAL COMMIT

---
+++ HW.py
@@ -0,0 +1,12 @@
+person_name=input('tell your name-')
+goal=input('what goal you want to improve-')
+target_month=input('tell the month YOU WANT TO REACH IT BY-')
+
+daily_minutes=(30)
+
+print("\nMY DAILY GOAL PLANNER\n")
+
+print('name:',person_name)
+print('goal:',goal)
+print('target month:',target_month)
+print('daily minutes:',daily_minutes)
